06e4f412a53725ed2eeaaa45d7acef3d4a40b6a3
Svetlana <sveta.sharymova@gmail.com>
2023-10-28 01:47:14 +0400

    01 | add pg database support

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hotel</title>
+    </head>
+    <body>
+        <h1>Welcome, Guest!</h1>
+        </body>
+</html>
