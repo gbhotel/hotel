7278bd22c4f3475ae18f7775248a4b302ce16dfb
Konyaeva Olga <82508979+Konyaeva@users.noreply.github.com>
2023-10-30 23:10:38 +0300

    Revert "пробный пуш"

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Database\Seeders\rooms\CategoriesSeeder;
-use Database\Seeders\rooms\RoomsSeeder;
-use Database\Seeders\rooms\StatusSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,12 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            CategoriesSeeder::class,
-            StatusSeeder::class,
-            RoomsSeeder::class
-        ]);
-
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\RoomController;
 use App\Http\Controllers\RoomsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +19,4 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::get('admin/rooms', [RoomsController::class, 'getRooms'])->name('admin.rooms');
-Route::get('admin/room/{id}/get-room', [RoomController::class, 'getRoom'])->name('admin.room.get-room');
-Route::put('admin/room/book-room', [RoomController::class, 'bookRoom'])->name('admin.room.book-room');
-Route::put('admin/room/cancel-book-room', [RoomController::class, 'cancelBookRoom'])->name('admin.room.cancel-book-room');
-Route::post('admin/room/check-in-room', [RoomController::class, 'checkInRoom'])->name('admin.room.check-in-room');
-Route::put('admin/room/eviction-from-room', [RoomController::class, 'evictionFromRoom'])->name('admin.eviction-from-room');
+Route::get('/rooms', [RoomsController::class, 'getRooms'])->name('rooms');
