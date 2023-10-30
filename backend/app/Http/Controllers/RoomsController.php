3649b1a49cc9f7ad4208e799814dc748adcb8471
AlexS <ashilov84@ya.ru>
2023-10-30 13:26:44 +0500

    выполнил задачу: Исправить контроллер RoomsController

--- backend/app/Http/Controllers/RoomsController.php
+++ backend/app/Http/Controllers/RoomsController.php
@@ -8,7 +8,22 @@ class RoomsController extends Controller
 {
     public function getRooms ()
     {
-        $rooms = DB::table("rooms")->get(['id', 'number', 'status']);
-        return response()->json($rooms);
+        $rooms = DB::table("rooms")->get(['id', 'number', 'id_status']);
+        $status = DB::table('statuses')->get();
+
+        $data = [];
+        foreach ($rooms as $key => $room)
+        {
+            $data[$key]['id'] = $room->id;
+            $data[$key]['number'] = $room->number;
+            foreach ($status as $st)
+            {
+                if($st->id == $room->id_status){
+                    $data[$key]['status'] = $st->status;
+                    break;
+                }
+            }
+        }
+        return response()->json($data);
     }
 }
