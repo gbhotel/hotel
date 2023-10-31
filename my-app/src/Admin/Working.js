f54d3a9ec241e58ae72d971fb7bc6cd1ab0e3df1
AlexS <ashilov84@ya.ru>
2023-10-31 17:56:29 +0500

    Ярослав добавил роуты в react

---
+++ my-app/src/Admin/Working.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+export default class Working extends Component {
+    render() {
+        return (
+            <div>
+                Рабочие
+            </div>
+        )
+    }
+}
